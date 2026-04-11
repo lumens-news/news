4c55f100c77b28e3c344cb95b8a7ece0709c7d68
Lucky Ivanius <luckzivanius@gmail.com>
2026-04-12 02:59:31 +0700

    feat: add passthrough evaluator check

--- packages/api/src/middlewares/is-evaluator.ts
+++ packages/api/src/middlewares/is-evaluator.ts
@@ -6,9 +6,32 @@ import { buildError } from "../utils/error";
 export const onlyEvaluatorErrorCode = "only_evaluator";
 export const onlyEvaluatorErrorMessage = "Only evaluator";
 
-export const isEvaluator = <TEvaluatorAddress extends string>(address?: TEvaluatorAddress) =>
-  createMiddleware<Env>(async (c, next) => {
-    if (c.env.EVALUATOR_AGENT_ADDRESS !== address) return c.json(buildError(onlyEvaluatorErrorCode, onlyEvaluatorErrorMessage), 403);
+export type EvaluatorVariables = {
+  Variables: {
+    isEvaluator: boolean;
+  };
+};
 
+export type IsEvaluatorOption = {
+  passthrough: boolean;
+};
+
+export const isEvaluator = <TEvaluatorAddress extends string>(address?: TEvaluatorAddress, options?: IsEvaluatorOption) =>
+  createMiddleware<Env & EvaluatorVariables>(async (c, next) => {
+    const { passthrough = false } = options ?? {};
+
+    if (passthrough) {
+      c.set("isEvaluator", c.env.EVALUATOR_AGENT_ADDRESS === address);
+
+      return next();
+    }
+
+    if (c.env.EVALUATOR_AGENT_ADDRESS !== address) {
+      c.set("isEvaluator", false);
+
+      return c.json(buildError(onlyEvaluatorErrorCode, onlyEvaluatorErrorMessage), 403);
+    }
+
+    c.set("isEvaluator", true);
     return next();
   });
